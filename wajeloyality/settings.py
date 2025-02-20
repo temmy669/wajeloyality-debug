896514211fde354b765fd252a3d0db68abf5dc36
Favour Adebose <favouradebose@gmail.com>
2025-02-20 10:02:06 +0100

    fixed static file errors

--- wajeloyality/settings.py
+++ wajeloyality/settings.py
@@ -214,10 +214,8 @@ SIMPLE_JWT = {
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 
-STATIC_ROOT = ''
-
-
-
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 FILE_UPLOAD_HANDLERS = ("django_excel.ExcelMemoryFileUploadHandler",
                         "django_excel.TemporaryExcelFileUploadHandler")
