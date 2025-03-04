00ba93d46e76bcd1090b73acf4501c4b5e71d63c
Favour Adebose <favouradebose@gmail.com>
2025-03-04 16:01:22 +0100

    added whitenoise module

--- wajeloyality/settings.py
+++ wajeloyality/settings.py
@@ -72,6 +72,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django.middleware.common.BrokenLinkEmailsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
  ]
 
 CORS_ORIGIN_ALLOW_ALL = True
