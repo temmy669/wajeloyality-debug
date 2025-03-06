14522f666af61ac36f41ea1f210622f884ef99e2
Nwokocha Maruche <nwokocha.maruche@gmail.com>
2025-03-06 12:11:45 +0100

    Role Base Access Control
    implemented a custom permission for which accountant can only view voucher

---
+++ wajecrm/permissions.py
@@ -0,0 +1,18 @@
+from rest_framework import permissions
+
+
+class VoucherPermission(permissions.BasePermission):
+    def has_permission(self, request, view):
+        """Ensure user is authenticated"""
+        return request.user and  request.user.is_authenticated
+        
+
+    def has_object_permission(self, request, view):
+        """Ensure account can only view the Voucher endpoint. """
+        if request.user.role == 'accountant' and \
+        request.method not in permissions.SAFE_METHODS:
+            return False
+        return True
+            
+
+
