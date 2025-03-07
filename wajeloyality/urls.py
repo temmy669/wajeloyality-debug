a937ca1746964a7d989e9b0329cc2ca940b7b70b
Nwokocha Maruche <nwokocha.maruche@gmail.com>
2025-03-07 04:57:24 +0100

    --no-edit

--- wajeloyality/urls.py
+++ wajeloyality/urls.py
@@ -19,13 +19,12 @@ from django.conf.urls import url,include
 from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
 
 spectacular_urls = [
-    # YOUR PATTERNS
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
-    # Optional UI:
     path('swagger', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('redoc', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 ]
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path(r'^', include('wajecrm.urls')),
-    ]
+    ] + spectacular_urls
