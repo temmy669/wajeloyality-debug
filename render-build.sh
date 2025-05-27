47cbceb710671b3aa78e8fd2c84046f948e69e2b
temmy669 <favouradebose@gmail.com>
2025-05-27 14:59:53 +0100

    update render-build script

--- render-build.sh
+++ render-build.sh
@@ -4,6 +4,9 @@ set -o errexit
 # Install system dependencies for wkhtmltopdf
 apt-get update && apt-get install -y wkhtmltopdf
 
+# Install Python dependencies
+pip install -r requirements.txt
+
 # Collect static files and run migrations
 python manage.py collectstatic --noinput
 python manage.py migrate
