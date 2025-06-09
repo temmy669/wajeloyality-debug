da4ec906782b7fbf0b44c7c92c04b68fb4f07d24
temmy669 <favouradebose@gmail.com>
2025-06-09 03:41:39 -0700

    updated settings.py

--- wajeloyality/settings.py
+++ wajeloyality/settings.py
@@ -30,7 +30,11 @@ SECRET_KEY = config('DJANGO_SECRET_KEY', default='secret_key')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['199.192.28.167',
+                 'wajeloyality.website', 'localhost:8080', '127.0.0.1', 
+                'wajeloyalityapitest.onrender.com',
+                  'wajeloyalityapitest-2.onrender.com', 'wajeloyality.marketsquareng.website']
+
 
 SETTINGS_PATH = settings.BASE_DIR
 
@@ -114,21 +118,22 @@ DATABASES = {
             'init_command': "SET SESSION sql_mode='STRICT_TRANS_TABLES'",
         },
     },
-    # 'loyalty': {
-    #     'ENGINE': 'sql_server.pyodbc',
-    #     'NAME': config('LOYALTY_DB_NAME', default='your_db_name'),
-    #     'USER': config('LOYALTY_DB_USER', default='your_db_name'),
-    #     'PASSWORD': config('LOYALTY_DB_PASSWORD', default='your_db_password'),
-    #     'HOST': config('LOYALTY_DB_HOST', default='your_db_host'),
-    #     'PORT': config('LOYALTY_DB_PORT', default='1433'),
-    #     'OPTIONS': {
-    #         'driver': config('LOYALTY_DB_DRIVER', default='driver'),
-    #         'unicode_results': True,
-    #         'host_is_server': True,
-    #         'autocommit': True,
-    #         'extra_params': 'tds_version=7.3;',
-    #     },
+    'loyalty': {
+        'ENGINE': 'sql_server.pyodbc',
+        'NAME': config('LOYALTY_DB_NAME', default='your_db_name'),
+        'USER': config('LOYALTY_DB_USER', default='your_db_name'),
+        'PASSWORD': config('LOYALTY_DB_PASSWORD', default='your_db_password'),
+        'HOST': config('LOYALTY_DB_HOST', default='your_db_host'),
+        'PORT': config('LOYALTY_DB_PORT', default='1433'),
+        'OPTIONS': {
+            'driver': config('LOYALTY_DB_DRIVER', default='driver'),
+            'unicode_results': True,
+            'host_is_server': True,
+            'autocommit': True,
+            'extra_params': 'tds_version=7.3;',
+        },
     }
+}
 
 
 # Use SQLite for testing
